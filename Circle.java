8882015ea243b21c2ded664f7a5ae10bdaff17d5
Carol Wang <cwang3@stuy.edu>
2015-12-11 02:04:01 -0500

    implemented Area

--- Circle.java
+++ Circle.java
@@ -1,4 +1,4 @@
-public class Circle extends Shape{
+public class Circle extends Shape implements Area{
 
     private double radius;
 
@@ -13,6 +13,10 @@ public class Circle extends Shape{
 
     public String toString(){
 	return "Circle "+getName()+" with radius of "+getRadius();
+    }
+
+    public double getArea(){
+	return Math.PI*Math.pow(radius,2);
     }
 
 }
